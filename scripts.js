592bf27b6f4713c398ef4d9da897eb011677364b
BuildTools <unconfigured@null.spigotmc.org>
2024-08-09 10:43:03 +0200

    added transition to filter-popup and made clicking outside of the popup close it

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
-function toggleFilter(state) {
-    if (state === true && document.getElementById("filter-category-menu").style.visibility === "hidden") {
-        document.getElementById("filter-category-menu").style.visibility = "visible"
+function toggleFilter() {
+    if (!(document.getElementById("filter-category-menu").classList.contains("showPopup"))) {
+        document.getElementById("filter-category-menu").classList.add("showPopup")
     } else {
-        document.getElementById("filter-category-menu").style.visibility = "hidden"
+        document.getElementById("filter-category-menu").classList.remove("showPopup")
     }
 }
+window.addEventListener('click', function(e){
+    if (!(document.getElementById("filter-category-menu").contains(e.target)) && !(document.getElementById("filter-button").contains(e.target))) {
+        document.getElementById("filter-category-menu").classList.remove("showPopup")
+    }
+});
